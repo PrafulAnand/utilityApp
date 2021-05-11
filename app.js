a2d61dcdaa9b73389a756860590d21a9f9059502
PrafulAnand <prafulanand724@gmail.com>
2021-05-12 04:03:25 +0530

    updated

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const encode = document.querySelector("#encode");
 const decode = document.querySelector("#decode");
 const reset = document.querySelector("#reset");
 const base64Encode = document.querySelector("#base64Encode");
-const base64Decode = document.querySelector("#base64Decode")
+const base64Decode = document.querySelector("#base64Decode");
+const generateMD5Hash = document.querySelector("#generateMD5Hash");
+const generateSHA1Hash = document.querySelector("#generateSHA1Hash");
+const generateSHA256Hash = document.querySelector("#generateSHA256Hash");
+const generateSHA512Hash = document.querySelector("#generateSHA512Hash");
+
+
+/*encode decode url */
+
 if (typeof (encode) != 'undefined' && encode != null) {
     encode.onclick = () => {
         if (encodeDecodeInput.value !== "") {
@@ -26,6 +34,7 @@ if (typeof (decode) != 'undefined' && decode != null) {
     }
 }
 
+/*reset */
 if (typeof (reset) != 'undefined' && reset != null) {
     reset.onclick = () => {
         encodeDecodeOutput.value = "";
@@ -33,6 +42,8 @@ if (typeof (reset) != 'undefined' && reset != null) {
         encodeDecodeInput.value = "";
     }
 }
+
+/*base64 encode decode */
 
 if (typeof (base64Encode) != 'undefined' && base64Encode != null) {
     base64Encode.onclick = () => {
@@ -49,9 +60,55 @@ if (typeof (base64Decode) != 'undefined' && base64Decode != null) {
         if (encodeDecodeInput.value !== "") {
             encodeDecodeOutput.disabled = false;
             encodeDecodeOutput.value = atob(encodeDecodeInput.value);
-                ;
+            ;
         } else {
             alert("Please enter encoded base64 value to decode");
+        }
+    }
+}
+
+/*hash generators */
+
+if (typeof (generateMD5Hash) != 'undefined' && generateMD5Hash != null) {
+    generateMD5Hash.onclick = () => {
+        if (encodeDecodeInput.value !== "") {
+            encodeDecodeOutput.disabled = false;
+            encodeDecodeOutput.value = CryptoJS.MD5(encodeDecodeInput.value);
+        } else {
+            alert("Please enter a value to generate MD5 hash");
+        }
+    }
+}
+if (typeof (generateSHA1Hash) != 'undefined' && generateSHA1Hash != null) {
+    generateSHA1Hash.onclick = () => {
+        if (encodeDecodeInput.value !== "") {
+            encodeDecodeOutput.disabled = false;
+            encodeDecodeOutput.value = CryptoJS.SHA1(encodeDecodeInput.value);
+            ;
+        } else {
+            alert("Please enter a value to generate SHA-1 hash");
+        }
+    }
+}
+
+if (typeof (generateSHA256Hash) != 'undefined' && generateSHA256Hash != null) {
+    generateSHA256Hash.onclick = () => {
+        if (encodeDecodeInput.value !== "") {
+            encodeDecodeOutput.disabled = false;
+            encodeDecodeOutput.value = CryptoJS.SHA256(encodeDecodeInput.value);
+        } else {
+            alert("Please enter a value to generate SHA-256 hash");
+        }
+    }
+}
+if (typeof (generateSHA512Hash) != 'undefined' && generateSHA512Hash != null) {
+    generateSHA512Hash.onclick = () => {
+        if (encodeDecodeInput.value !== "") {
+            encodeDecodeOutput.disabled = false;
+            encodeDecodeOutput.value = CryptoJS.SHA512(encodeDecodeInput.value);
+            ;
+        } else {
+            alert("Please enter a value to generate SHA-512 hash");
         }
     }
 }
