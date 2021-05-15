7d8df07607b4a6a764e7906d468753551f2b4192
PrafulAnand <prafulanand724@gmail.com>
2021-05-15 18:22:50 +0530

    number base converter logic implemented

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const encodeDecodeInput = document.querySelector("#encodeDecodeInput");
-const encodeDecodeOutput = document.querySelector("#encodeDecodeOutput");
+const utilityConverterInput = document.querySelector("#utilityConverterInput");
+const utilityConverterOutput = document.querySelector("#utilityConverterOutput");
 const encode = document.querySelector("#encode");
 const decode = document.querySelector("#decode");
 const reset = document.querySelector("#reset");
@@ -11,13 +11,27 @@ const generateSHA256Hash = document.querySelector("#generateSHA256Hash");
 const generateSHA512Hash = document.querySelector("#generateSHA512Hash");
 
 
+const binaryToDecimal = document.querySelector("#binaryDecimal");
+const binaryToOctal = document.querySelector("#binaryOctal");
+const binaryToHex= document.querySelector("#binaryHex");
+const decimalToBinary= document.querySelector("#decimalBinary");
+const decimalToOctal= document.querySelector("#decimalOctal");
+const decimaltoHex= document.querySelector("#decimalHex");
+const hexToBinary= document.querySelector("#hexBinary");
+const hextoDecimal= document.querySelector("#hexDecimal");
+const hexToOctal= document.querySelector("#hexOctal");
+const octalToBinary= document.querySelector("#octalBinary");
+const octalToDecimal= document.querySelector("#octalDecimal");
+const octalToHex= document.querySelector("#octalHex");
+
+
 /*encode decode url */
 
 if (typeof (encode) != 'undefined' && encode != null) {
     encode.onclick = () => {
-        if (encodeDecodeInput.value !== "") {
-            encodeDecodeOutput.disabled = false;
-            encodeDecodeOutput.value = encodeURIComponent(encodeDecodeInput.value);
+        if (utilityConverterInput.value !== "") {
+            utilityConverterOutput.disabled = false;
+            utilityConverterOutput.value = encodeURIComponent(utilityConverterInput.value);
         } else {
             alert("Please enter a Url value to encode");
         }
@@ -25,9 +39,9 @@ if (typeof (encode) != 'undefined' && encode != null) {
 }
 if (typeof (decode) != 'undefined' && decode != null) {
     decode.onclick = () => {
-        if (encodeDecodeInput.value !== "") {
-            encodeDecodeOutput.disabled = false;
-            encodeDecodeOutput.value = decodeURIComponent(encodeDecodeInput.value);
+        if (utilityConverterInput.value !== "") {
+            utilityConverterOutput.disabled = false;
+            utilityConverterOutput.value = decodeURIComponent(utilityConverterInput.value);
         } else {
             alert("Please enter encoded Url value to decode");
         }
@@ -37,9 +51,9 @@ if (typeof (decode) != 'undefined' && decode != null) {
 /*reset */
 if (typeof (reset) != 'undefined' && reset != null) {
     reset.onclick = () => {
-        encodeDecodeOutput.value = "";
-        encodeDecodeOutput.disabled = true;
-        encodeDecodeInput.value = "";
+       utilityConverterOutput.value = "";
+       utilityConverterOutput.disabled = true;
+       utilityConverterInput.value = "";
     }
 }
 
@@ -47,9 +61,9 @@ if (typeof (reset) != 'undefined' && reset != null) {
 
 if (typeof (base64Encode) != 'undefined' && base64Encode != null) {
     base64Encode.onclick = () => {
-        if (encodeDecodeInput.value !== "") {
-            encodeDecodeOutput.disabled = false;
-            encodeDecodeOutput.value = btoa(encodeDecodeInput.value);
+        if (utilityConverterInput.value !== "") {
+            utilityConverterOutput.disabled = false;
+            utilityConverterOutput.value = btoa(utilityConverterInput.value);
         } else {
             alert("Please enter a vale to encode to base64");
         }
@@ -57,10 +71,9 @@ if (typeof (base64Encode) != 'undefined' && base64Encode != null) {
 }
 if (typeof (base64Decode) != 'undefined' && base64Decode != null) {
     base64Decode.onclick = () => {
-        if (encodeDecodeInput.value !== "") {
-            encodeDecodeOutput.disabled = false;
-            encodeDecodeOutput.value = atob(encodeDecodeInput.value);
-            ;
+        if (utilityConverterInput.value !== "") {
+            utilityConverterOutput.disabled = false;
+            utilityConverterOutput.value = atob(utilityConverterInput.value);
         } else {
             alert("Please enter encoded base64 value to decode");
         }
@@ -71,9 +84,9 @@ if (typeof (base64Decode) != 'undefined' && base64Decode != null) {
 
 if (typeof (generateMD5Hash) != 'undefined' && generateMD5Hash != null) {
     generateMD5Hash.onclick = () => {
-        if (encodeDecodeInput.value !== "") {
-            encodeDecodeOutput.disabled = false;
-            encodeDecodeOutput.value = CryptoJS.MD5(encodeDecodeInput.value);
+        if (utilityConverterInput.value !== "") {
+            utilityConverterOutput.disabled = false;
+            utilityConverterOutput.value = CryptoJS.MD5(utilityConverterInput.value);
         } else {
             alert("Please enter a value to generate MD5 hash");
         }
@@ -81,10 +94,9 @@ if (typeof (generateMD5Hash) != 'undefined' && generateMD5Hash != null) {
 }
 if (typeof (generateSHA1Hash) != 'undefined' && generateSHA1Hash != null) {
     generateSHA1Hash.onclick = () => {
-        if (encodeDecodeInput.value !== "") {
-            encodeDecodeOutput.disabled = false;
-            encodeDecodeOutput.value = CryptoJS.SHA1(encodeDecodeInput.value);
-            ;
+        if (utilityConverterInput.value !== "") {
+            utilityConverterOutput.disabled = false;
+            utilityConverterOutput.value = CryptoJS.SHA1(utilityConverterInput.value);
         } else {
             alert("Please enter a value to generate SHA-1 hash");
         }
@@ -93,9 +105,9 @@ if (typeof (generateSHA1Hash) != 'undefined' && generateSHA1Hash != null) {
 
 if (typeof (generateSHA256Hash) != 'undefined' && generateSHA256Hash != null) {
     generateSHA256Hash.onclick = () => {
-        if (encodeDecodeInput.value !== "") {
-            encodeDecodeOutput.disabled = false;
-            encodeDecodeOutput.value = CryptoJS.SHA256(encodeDecodeInput.value);
+        if (utilityConverterInput.value !== "") {
+            utilityConverterOutput.disabled = false;
+            utilityConverterOutput.value = CryptoJS.SHA256(utilityConverterInput.value);
         } else {
             alert("Please enter a value to generate SHA-256 hash");
         }
@@ -103,12 +115,121 @@ if (typeof (generateSHA256Hash) != 'undefined' && generateSHA256Hash != null) {
 }
 if (typeof (generateSHA512Hash) != 'undefined' && generateSHA512Hash != null) {
     generateSHA512Hash.onclick = () => {
-        if (encodeDecodeInput.value !== "") {
-            encodeDecodeOutput.disabled = false;
-            encodeDecodeOutput.value = CryptoJS.SHA512(encodeDecodeInput.value);
+        if (utilityConverterInput.value !== "") {
+            utilityConverterOutput.disabled = false;
+            utilityConverterOutput.value = CryptoJS.SHA512(utilityConverterInput.value);
             ;
         } else {
             alert("Please enter a value to generate SHA-512 hash");
+        }
+    }
+}
+
+/*Binary to Decimal */
+if (typeof (binaryToDecimal) != 'undefined' && binaryToDecimal != null) {
+    binaryToDecimal.onclick = () => {
+        if (utilityConverterInput.value !== "") {
+            utilityConverterOutput.disabled = false;
+            utilityConverterOutput.value = parseInt(utilityConverterInput.value,2);
+        }  else {
+            alert("Please enter a Binary value to get Decimal Value");
+        }
+    }
+}
+
+/*Binary to hex */
+if (typeof (binaryToHex) != 'undefined' && binaryToHex != null) {
+    binaryToHex.onclick = () => {
+        if (utilityConverterInput.value !== "") {
+            utilityConverterOutput.disabled = false;
+            utilityConverterOutput.value = parseInt(utilityConverterInput.value,2).toString(16);
+        }  else {
+            alert("Please enter a Binary value to get hex Value");
+        }
+    }
+}
+
+/*Binary to octal */
+if (typeof (binaryToOctal) != 'undefined' && binaryToOctal != null) {
+    binaryToOctal.onclick = () => {
+        if (utilityConverterInput.value !== "") {
+            utilityConverterOutput.disabled = false;
+            utilityConverterOutput.value = parseInt(utilityConverterInput.value,2).toString(8);
+        }  else {
+            alert("Please enter a Binary value to get octal Value");
+        }
+    }
+}
+
+/*hex to Binary */
+if (typeof (hexToBinary) != 'undefined' && hexToBinary != null) {
+    hexToBinary.onclick = () => {
+        if (utilityConverterInput.value !== "") {
+            utilityConverterOutput.disabled = false;
+            utilityConverterOutput.value = parseInt(utilityConverterInput.value,16).toString(2);
+        }  else {
+            alert("Please enter a Hex value to get Binary Value");
+        }
+    }
+}
+
+
+/*hex to Decimal */
+if (typeof (hextoDecimal) != 'undefined' && hextoDecimal != null) {
+    hextoDecimal.onclick = () => {
+        if (utilityConverterInput.value !== "") {
+            utilityConverterOutput.disabled = false;
+            utilityConverterOutput.value = parseInt(utilityConverterInput.value,16);
+        }  else {
+            alert("Please enter a Hex value to get Decimal Value");
+        }
+    }
+}
+
+/*hex to Octal */
+if (typeof (hextoOctal) != 'undefined' && hextoOctal != null) {
+    hextoOctal.onclick = () => {
+        if (utilityConverterInput.value !== "") {
+            utilityConverterOutput.disabled = false;
+            utilityConverterOutput.value = parseInt(utilityConverterInput.value,16).toString(8);
+        }  else {
+            alert("Please enter a Hex value to get Decimal Value");
+        }
+    }
+}
+
+/*octal to Decimal */
+if (typeof (octalToDecimal) != 'undefined' && octalToDecimal != null) {
+    octalToDecimal.onclick = () => {
+        if (utilityConverterInput.value !== "") {
+            utilityConverterOutput.disabled = false;
+            utilityConverterOutput.value = parseInt(utilityConverterInput.value,8);
+        } else {
+            alert("Please enter a octal value to get Decimal Value");
+        }
+    }
+}
+
+/*octal to Binary */
+if (typeof (octalToBinary) != 'undefined' && octalToBinary != null) {
+    octalToBinary.onclick = () => {
+        if (utilityConverterInput.value !== "") {
+            utilityConverterOutput.disabled = false;
+            utilityConverterOutput.value = parseInt(utilityConverterInput.value,8).toString(2);
+        } else {
+            alert("Please enter a octal value to get Binary Value");
+        }
+    }
+}
+
+/*octal to hex */
+if (typeof (octalToHex) != 'undefined' && octalToHex != null) {
+    octalToHex.onclick = () => {
+        if (utilityConverterInput.value !== "") {
+            utilityConverterOutput.disabled = false;
+            utilityConverterOutput.value = parseInt(utilityConverterInput.value,8).toString(16);
+        } else {
+            alert("Please enter a octal value to get Hex Value");
         }
     }
 }
